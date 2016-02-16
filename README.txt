474b5a8d9703388523ae3ddbfde329bf56ab0b06
Thankful Cromartie <thankful@virginia.edu>
2016-02-16 13:32:44 -0500

    stats c and python script added to wrap misc_utils' stats function

---
+++ README.txt
@@ -0,0 +1,2 @@
+to compile c as shared lib:
+
--- stats.py
+++ stats.py
@@ -2,21 +2,22 @@ import numpy as np
 import ctypes
 import matplotlib.pyplot as plt
 
-n = 100
+n = 10000
 mu = 0
 sig = 0.1
-x = np.random.normal(mu,sig,100)
+x = np.random.normal(mu,sig,n).astype(np.float32)
 print x
-m = 0
-v = 0
-s = 0
-k = 0
+m = ctypes.c_double()
+v = ctypes.c_double()
+s = ctypes.c_double()
+k = ctypes.c_double()
 
 stats = ctypes.cdll.LoadLibrary("./stats.so")
-xptr = x.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
-stats.stats(xptr, n, m, v, s, k)
-print 'mean = %d, variance = %d, skew = %d, kurtosis = %d' (m, v, s, k)
+xptr = x.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
 
-count, bins, ignored = plt.hist(x, 7, normed=True)
+stats.stats(xptr, n, ctypes.byref(m),ctypes.byref(v), ctypes.byref(s), ctypes.byref(k))
+print 'mean = %f, variance = %f, skew = %f, kurtosis = %f' %(m.value, v.value, s.value, k.value)
+
+count, bins, ignored = plt.hist(x, 50, normed=True)
 plt.plot(bins, 1/(sig * np.sqrt(2 * np.pi)) * np.exp(-(bins-mu)**2 / (2*sig**2)))
 plt.show()
